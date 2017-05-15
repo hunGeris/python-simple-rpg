07209f6faf7693023ee2f6ae8b7af76ed3fc1b41
Seton Brown <seton_brown@ziffdavis.com>
2017-05-15 09:23:52 -0400

    Initializing Person Class for the RPG

---
+++ classes/game.py
@@ -0,0 +1,32 @@
+import random
+
+
+# have no idea what this will be used for, looks like colors for the RPG.
+# TBD
+class bcolors:
+    HEADER: '\033[95m'
+    OKBLUE: '\033[94m'
+    OKGREEN: '\033[92m'
+    WARNING: '\033[93m'
+    FAIL: '\033[91m'
+    ENDC: '\033[0m'
+    BOLD: '\033[1m'
+    UNDERLINE: '\033[4m'
+
+
+# init the Person class
+class Person:
+    # annoying that we have to init all the items passed in, but normal to do so
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.actions =["Attack", "Magic"]
+
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
